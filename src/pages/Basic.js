823987f525687453900429a46ab8b50f812d272b
Matt <matt.rajkovic@magnolia-cms.com>
2022-10-14 09:23:33 +0200

    commit message

--- src/pages/Basic.js
+++ src/pages/Basic.js
@@ -6,21 +6,22 @@ const Basic = props => {
 
   return (
     <div className="Basic">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'></link>
       <div className="hint">[Basic Page]</div>
       <h1>{title || props.metadata['@name']}</h1>
 
       <main>
-        <div className="hint">[Main Area]</div>         
-        {main && <EditableArea className="Area" content={main} />}  
+        <div className="hint">[Main Area]</div>
+        {main && <EditableArea className="Area" content={main} />}
       </main>
 
       <div className="Extras" >
         <div className="hint">[Sercondary Area]</div>
-        {extras && <EditableArea className="Area" content={extras} />}  
+        {extras && <EditableArea className="Area" content={extras} />}
         {/* <button>Contact</button> */}
-      </div>    
+      </div>
     </div>
-  ) 
+  )
 };
 
 export default Basic;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,6 +10,7 @@ const Contact = props => {
 
   return (
     <div className="Contact">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'></link>
       <h2  className="hint">[Contact Page]</h2>
       <div className="box" style={boxStyle}>
         <h1>{title || 'Nulla vitae elit libero, a pharetra augue.'}</h1>
